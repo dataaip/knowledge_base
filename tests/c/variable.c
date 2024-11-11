4dec68dde7bdb60bd05305f3234fc4a4e59fd261
brightl <brightl3016@outlook.com>
2024-11-11 19:57:39 +0800

    cl-day10：variable character add

--- tests/c/variable.c
+++ tests/c/variable.c
@@ -1,83 +1,25 @@
-#include <math.h>
+#include <locale.h>
 #include <stdio.h>
-#include <stdlib.h>
-
-#define PRINT_INT128(n)                                                        \
-  do {                                                                         \
-    if ((n) == 0) {                                                            \
-      putchar('0');                                                            \
-    } else {                                                                   \
-      char queue[40];                                                          \
-      if ((n) < 0) {                                                           \
-        putchar('-');                                                          \
-      }                                                                        \
-      int i = 0;                                                               \
-      __int128 temp = (n);                                                     \
-      while (!(temp == 0)) {                                                   \
-        queue[i++] = '0' + abs((int)(temp % 10));                              \
-        temp /= 10;                                                            \
-      }                                                                        \
-      while (i > 0) {                                                          \
-        putchar(queue[--i]);                                                   \
-      }                                                                        \
-    }                                                                          \
-    putchar('\n');                                                             \
-  } while (0)
-
-// 循环 -> 优化递归
-int print_int128(__int128 data) {
-  if (data == 0) {
-    putchar('0');
-    goto end;
-  }
-  char queue[40];
-  if (data < 0) {
-    putchar('-');
-  }
-  int i = 0;
-  __int128 temp = data;
-  while (!(temp == 0)) {
-    queue[i++] = '0' + abs((int)(temp % 10));
-    temp /= 10;
-  }
-  while (i > 0) {
-    putchar(queue[--i]);
-  }
-end:
-  putchar('\n');
-  return 0;
-}
-// 递归 -> 不能优化为尾递归
-void print_int128_st1(__int128 temp) {
-  if (temp == 0) {
-    return;
-  }
-  print_int128_st1(temp / 10);
-  putchar(abs((int)(temp % 10)) + '0');
-}
-int print_int128_st0(__int128 data) {
-  if (data == 0) {
-    putchar('0');
-    goto end;
-  }
-  if (data < 0) {
-    putchar('-');
-  }
-  __int128 temp = data;
-  print_int128_st1(temp);
-end:
-  putchar('\n');
-  return 0;
-}
+#include <wchar.h>
 
 int main(void) {
+
+  setlocale(LC_ALL, "en_US.UTF-8");
+  fflush(stdout);
+
+  int direction = fwide(stdout, 1); 
+
+  wprintf(L"fwide is %d.\n",direction);
+
   printf("variable test...\n\n");
 
-  __int128_t maximum = ((__int128_t)1 << 127) - 1;
-  __int128_t minimum = -((__int128_t)1 << 127);
-  __int128_t zero = 0;
-  PRINT_INT128(maximum);
-  PRINT_INT128(minimum);
-  PRINT_INT128(zero);
+  wprintf(L"天生我材必有用");
+  wchar_t sheep = L'A';
+  wchar_t horse[] = L"Hello, 世界";
+  wprintf(L"sheep is %lc\n", sheep);
+  wprintf(L"horse is %ls\n", horse);
+
+  printf("variable test...\n\n");
+
   return 0;
 }
