e00956c7dc4b5fab3affd12308dda284aacf10ce
miguelau <miguelau3016@outlook.com>
2025-08-14 18:47:33 +0800

    gfalgs提交

--- reference/tour_c_cpp/cpp/progcc.cpp
+++ reference/tour_c_cpp/cpp/progcc.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #include "fmt/base.h"
 #include "toolsmath/toolsmath.h"
@@ -7,10 +8,33 @@
 #define SPDLOG_FMT_EXTERNAL // 定义不使用 spdlog 内部的 fmt 使用外部自定义引入的
 #include "spdlog/spdlog.h"
 
-int main()
+#define STRIP_FLAG_HELP 1
+#include "gflags/gflags.h"
+
+DEFINE_bool(verbose, false, "Enable verbose output");
+DEFINE_string(config, "default.cfg", "Config file path");
+DEFINE_int32(port, 8080, "Server port");
+
+int main(int argc, char** argv)
 {
     ToolsMath math;
     Utils     utils;
+
+    // 设置自定义帮助信息
+    gflags::SetUsageMessage(
+        "A demo program using gflags\n"
+        "Usage: progcpp --config=<file> --port=<num> [--verbose]\n"
+        "Example: ./progcpp --config=my.cfg --port=8081 --verbose"
+    );
+
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+    if (FLAGS_verbose) {
+        std::cout << "Using config file: " << FLAGS_config << std::endl;
+    }
+    std::cout << "Server port: " << FLAGS_port << std::endl;
+
+    fmt::print("----------------------------\n");
 
 #ifdef Debug
     std::cout << "This is a Debug version." << std::endl;
